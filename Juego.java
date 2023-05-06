cc57640ab87dc9779a1e659e69040fed1cbb5f47
Roichu5k <116627095+Roichu5k@users.noreply.github.com>
2023-05-06 10:56:01 +0200

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -5,6 +5,7 @@
 
 package es.uvigo.esei.aed1.core;
 
+import es.uvigo.esei.aed1.core.Carta.Palo;
 import es.uvigo.esei.aed1.iu.IU;
 import java.util.Collection;
 import java.util.*;
@@ -12,6 +13,10 @@ import java.util.*;
 
 public class Juego {
     private final IU iu;
+    //Se crea la baraja
+    Baraja baraja = new Baraja();
+    //Se crea mesa
+    Mesa mesa = new Mesa();
 
 
     public Juego(IU iu) {
@@ -20,68 +25,137 @@ public class Juego {
 
     public void jugar() {
         
-        //Se crea la baraja
-        Baraja baraja = new Baraja();
-        //Se crea la mesa
-        Mesa mesa = new Mesa();
-
+        
+        
         //Se obtiene informacion de quien va a jugar
-        Collection listaJugadores = iu.pedirDatosJugadores();
-        Iterator it = listaJugadores.iterator();
-
+        Collection<String> listaJugadores = iu.pedirDatosJugadores();
+        Iterator<String> it = listaJugadores.iterator();
+        
         //Se crean los Jugadores
         List<Jugador> jugadores = new ArrayList<>();
-
         //Metemos a los jugadores en la lista
         for (int i = 0; i < listaJugadores.size(); i++) {
-            Jugador nuevoJugador = new Jugador(it.next().toString());
+            Jugador nuevoJugador = new Jugador(it.next());
             jugadores.add(nuevoJugador);
         }
-        //A partir de aqui comienza el bucle del juego
-        //Se baraja y se reparten las cartas entre los jugadores
-        repartirCartas(baraja, jugadores);
-
+        
+        
+        
         //Se enseñan las cartas de cada Jugador
         iu.mostrarJugadores(jugadores);
+        
+        //CONTADOR QUE VA A LLEVAR EL NUMERO DE PUNTOS
 
         //Se elije un jugador al azar para empezar la partida
         boolean hayGanador = false;
-        int posTurno = elegirJugador(jugadores);
-        int contador = posTurno;
-        //Aqui empieza el turno
-        do {
-            for (int i = 0; i < jugadores.size(); i++) {
-                mesa.toString();
-                if (tieneCartasValidas(jugadores.get(contador), mesa)) {
-                    int num;
-                    do {
-                        num = iu.leeNum("Que carta se quiere colocar?(0, 1, 2, 3...): ");
+        boolean asOros = false;
+        
+        
+        do{
 
-                    } while (!mesa.cartaValida(jugadores.get(contador).getMano().get(num)));
+           do {
+               
+               
+               //Se baraja y se reparten las cartas entre los jugadores
+               baraja.barajar();
+               for (int i = 0; i < 48 / jugadores.size(); i++) {
+                   for (int j = 0; j < jugadores.size(); j++) {
+                       jugadores.get(j).añadirCartas(baraja.sacarCarta());
+                   }
+               }
 
-                    if (jugadores.get(contador).getMano().isEmpty()) {
-                        hayGanador = true;
-                        iu.mostrarMensaje("El jugador", contador, "se ha quedado sin cartas, por tanto ha ganado.");
-                        break;
-                    } else {
-                        siguienteJugador(contador, jugadores.size());
+               int contador = elegirJugador(jugadores);
+               iu.mostrarJugador(jugadores.get(contador));
+
+            if (jugadores.get(contador).tieneCartasValidas(mesa)) {
+                int num;
+                
+                do {
+                    
+                    do{
+                        num = iu.leeNum("Seleccion una numero de su mano (0,1,2..)");
+                    }while(num < 0 || num > jugadores.get(contador).getMano().size()-1);
+                    
+                    
+                    
+                    if (!mesa.cartaValida(jugadores.get(contador).getMano().get(num))) {
+                        iu.mostrarMensaje("Carta incorrecta");
                     }
-                }else{
-                    iu.mostrarMensaje("No tienes cartas válidas para echar, se saltará al siguiente jugador.");
-                    siguienteJugador(contador, jugadores.size());
+                    
+                } while (!mesa.cartaValida(jugadores.get(contador).getMano().get(num)));
+                
+                
+                iu.mostrarMensaje("Introduciendo Carta...");
+                
+                
+                //SI INTRODUCE EL AS DE OROS
+                if(jugadores.get(contador).getMano().get(num).getPalo().equals(Palo.Oros) && jugadores.get(contador).getMano().get(num).getNumero()==1){
+                    jugadores.get(contador).setPuntosAcumulados(jugadores.get(contador).getPuntosAcumulados()+1);
+                    asOros = true;
+                    
+                }
+
+                mesa.insertarCarta(jugadores.get(contador).getMano().get(num));
+                
+                
+                //JUGADOR SE QUEDA SIN CARTAS
+                if(jugadores.get(contador).size() == 0){
+                    hayGanador = true;
+                    jugadores.get(contador-1).setPuntosAcumulados(jugadores.get(contador-1).getPuntosAcumulados()+1);
+                }
+
+                System.out.println("MESA: \n");
+
+                iu.mostrarMensaje(mesa.toString());
+                
+                //CONTROL DE CONTADOR
+                if (contador == jugadores.size() - 1) {
+                    contador = 0;
+                } else {
+                    contador++;
+                }
+                siguienteJugador(contador, jugadores.size());
+
+            } else {
+                iu.mostrarMensaje("No tienes cartas válidas para echar, se saltará al siguiente jugador.");
+                
+                if (contador == jugadores.size() - 1) {
+                    contador = 0;
+                } else {
+                    contador++;
+                }
+                
+                iu.mostrarMensaje(mesa.toString());
+            }
+            
+            
+            //ELIMINAR CARTAS DE TODOS LO JUGADORES PARA LA BARAJA
+            for(int i = 0; i < jugadores.size(); i++){
+                if(!jugadores.get(i).getMano().isEmpty()){
+                    for(int j= 0; j < jugadores.get(i).size(); j++){
+                        
+                    }
                 }
             }
-        } while (hayGanador == false);
+            
+            
+        } while (!hayGanador);
+           
+        }while(!asOros);
+           
+        
+        //iu.mostrarMensaje("El ganador es, ", jugadores.get(contador-1).getNombre());
+
     }
     
-    public void repartirCartas(Baraja baraja, List<Jugador> jugadores){
-        baraja.barajar();
-        for (int i = 0; i < 48 / jugadores.size(); i++) {
-            for (int j = 0; j < jugadores.size(); j++) {
-                jugadores.get(j).añadirCartas(baraja.sacarCarta());
-            }
-        }
-    }
+//    public boolean tieneCartasValidas (Jugador jugador, Mesa mesa){
+//        for(int i = 0; i < jugador.getMano().size(); i++){
+//            if(mesa.cartaValida(jugador.getMano().get(i))){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
     
     public int elegirJugador(List<Jugador> jugadores){
         int posTurno = (int) (Math.random() * jugadores.size());
@@ -91,21 +165,17 @@ public class Juego {
         return posTurno;
     }
     
-    public boolean tieneCartasValidas(Jugador jugador, Mesa mesa){
-        for (int i = 0; i < jugador.getMano().size(); i++) {
-            if(mesa.cartaValida(jugador.getMano().get(i))){
-                return true;
-            }
+    
+    public int siguienteJugador(int contador, int numJugadores) {
+
+        if (contador == numJugadores - 1) {
+            contador = 0;
+        } else {
+            contador++;
         }
-        return false;
+        return contador;
     }
 
-    public static void siguienteJugador(int contador, int numJugadores){
-        
-        if(contador == numJugadores){
-                        contador =0;
-                    }else{
-                        contador++;
-                    }
-    }
+
+
 }
